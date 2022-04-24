ad6d338d3662ed5a94e1b78b30f4bff720d47ec1
Tobias Messner <tobias.d.messner@gmail.com>
2022-04-24 19:29:45 +0200

    Fix regex

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ client.on('messageCreate', async (message) => {
     new Set(recentMessages
       .filter(recentMessage => recentMessage.author.id === message.author.id && recentMessage.content === message.content)
       .map(recentMessage => recentMessage.channel.id)).size;
-  console.log(similarMessageCount);
-  const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  const containsUrl = regexp.test(message.content.toLowerCase());
+  const containsUrl = message.content.match(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/)
+  console.log(`[zusorantispam] ${message.id} similar message count: ${similarMessageCount} | Contains URL: ${containsUrl}`);
 
   if (similarMessageCount >= 4 && containsUrl) {
     const webhookClient = new WebhookClient({ url: config[message.guild.id] });
